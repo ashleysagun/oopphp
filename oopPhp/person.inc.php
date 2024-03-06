832a4d4a546bcc09108fc83bd98af01d2e3401af
ashleysagun <148184755+ashleysagun@users.noreply.github.com>
2024-03-05 20:33:28 -0800

    Add files via upload

---
+++ oopPhp/person.inc.php
@@ -0,0 +1,24 @@
+<?php
+
+class Person {
+    private $first = "ashley";
+    private $last = "sagun";
+    private $age = "23";
+    private $occupation ="student";
+
+public function owner() {
+    $a = $this->occupation;
+return $a;
+}
+
+}
+
+
+
+class Pet {
+    // public function owner() {
+    //     $a = "Hello there!";
+    //     return $a;
+       
+    // }
+}
